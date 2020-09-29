542d6d5433e4233364418eb782a59b780c118d2b
Nikolas Barwicki <nikolas.barwicki@gmail.com>
2020-09-29 15:22:50 +0200

    Style user interface

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,174 @@
 import React from 'react';
 import './styles/main.css';
 
+import { WiSnow, WiDaySunnyOvercast, WiCloudy } from 'react-icons/wi';
+import Chart from './Chart';
+
 function App() {
   return (
-    <div className="h-64">
-      <div className="p-4 m-4 bg-green-600">
-        <h1 className="text-2xl font-bold text-white">Tailwind CSS Demo</h1>
+    <div className="flex min-h-screen">
+      <div className="flex justify-between flex-col w-1/6 bg-indigo-600 p-8">
+        <nav>
+          <span className="font-semibold text-white text-2xl tracking-widest">
+            METEOROLOG
+          </span>
+          <ul className="mt-16">
+            <li className="text-white text-xl font-semibold py-2 my-4 flex">
+              <svg
+                className="h-8 mr-3"
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 20 20"
+                fill="currentColor"
+              >
+                <path d="M3 4a1 1 0 011-1h12a1 1 0 011 1v2a1 1 0 01-1 1H4a1 1 0 01-1-1V4zM3 10a1 1 0 011-1h6a1 1 0 011 1v6a1 1 0 01-1 1H4a1 1 0 01-1-1v-6zM14 9a1 1 0 00-1 1v6a1 1 0 001 1h2a1 1 0 001-1v-6a1 1 0 00-1-1h-2z" />
+              </svg>
+              Dashboard
+            </li>
+            <li className="text-indigo-200 text-xl font-semibold py-2 my-4 flex hover:text-white">
+              <svg
+                className="h-8 mr-3"
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 20 20"
+                fill="currentColor"
+              >
+                <path
+                  fillRule="evenodd"
+                  d="M11.49 3.17c-.38-1.56-2.6-1.56-2.98 0a1.532 1.532 0 01-2.286.948c-1.372-.836-2.942.734-2.106 2.106.54.886.061 2.042-.947 2.287-1.561.379-1.561 2.6 0 2.978a1.532 1.532 0 01.947 2.287c-.836 1.372.734 2.942 2.106 2.106a1.532 1.532 0 012.287.947c.379 1.561 2.6 1.561 2.978 0a1.533 1.533 0 012.287-.947c1.372.836 2.942-.734 2.106-2.106a1.533 1.533 0 01.947-2.287c1.561-.379 1.561-2.6 0-2.978a1.532 1.532 0 01-.947-2.287c.836-1.372-.734-2.942-2.106-2.106a1.532 1.532 0 01-2.287-.947zM10 13a3 3 0 100-6 3 3 0 000 6z"
+                  clipRule="evenodd"
+                />
+              </svg>
+              Settings
+            </li>
+          </ul>
+        </nav>
+        <div className="bg-white rounded-lg  py-4 px-8">
+          <div>
+            <div className="flex mb-6">
+              <WiSnow className="text-6xl text-indigo-600 mb-1 mr-2" />
+              <div className="flex flex-col">
+                <span className="text-2xl font-semibold">Today</span>
+                <span className="text-xl font-semibold">11:44</span>
+                <span className="text font-semibold text-gray-600">
+                  Mon, 11 January
+                </span>
+              </div>
+            </div>
+            <div className="flex flex-col items-center">
+              <span className="text-4xl font-semibold">+4&deg;C</span>
+              <span className="text-2xl font-semibold">New York</span>
+              <span className="font-semibold  text-gray-600">
+                United States
+              </span>
+              <div className="w-full my-2">
+                <div>
+                  <div className="flex justify-between mt-3">
+                    <span className="font-semibold text-gray-600">
+                      Humidity:
+                    </span>
+                    <span className="font-semibold">70%</span>
+                  </div>
+                  <div className="h-1 relative max-w-xl rounded-full overflow-hidden mt-1">
+                    <div className="w-full h-full bg-gray-200 absolute" />
+                    <div
+                      className="h-full bg-orange-400 absolute"
+                      style={{ width: '70%' }}
+                    />
+                  </div>
+                </div>
+                <div>
+                  <div className="flex justify-between mt-3">
+                    <span className="font-semibold text-gray-600">
+                      Precipitation:
+                    </span>
+                    <span className="font-semibold">25%</span>
+                  </div>
+                  <div className="h-1 relative max-w-xl rounded-full overflow-hidden mt-1">
+                    <div className="w-full h-full bg-gray-200 absolute" />
+                    <div
+                      className="h-full bg-orange-400 absolute"
+                      style={{ width: '25%' }}
+                    />
+                  </div>
+                </div>
+                <div>
+                  <div className="flex justify-between mt-3">
+                    <span className="font-semibold text-gray-600">Wind:</span>
+                    <span className="font-semibold">4.5 km/h</span>
+                  </div>
+                  <div className="h-1 relative max-w-xl rounded-full overflow-hidden mt-1">
+                    <div className="w-full h-full bg-gray-200 absolute" />
+                    <div
+                      className="h-full bg-orange-400 absolute"
+                      style={{ width: '15%' }}
+                    />
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
-      <div className="p-4 m-4 bg-green-300 h-full">
-        <h2 className="text-green-900">Have much fun using Tailwind CSS</h2>
-        <button
-          type="button"
-          className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
-        >
-          My Tailwind Button
-        </button>
-      </div>
+      <main className="w-5/6 p-10 flex flex-col">
+        <section className="mb-8">
+          <h1 className="text-3xl font-bold text-gray-800 mb-4">
+            Weather Forecast
+          </h1>
+          <div>obrazki miast</div>
+        </section>
+        <section className="mb-8">
+          <h1 className="text-3xl font-bold text-gray-800 mb-4">London</h1>
+          <div className="flex flex-row">
+            <div className="flex flex-col items-center bg-gray-200 rounded-lg py-8 px-10 mr-12">
+              <WiSnow className="text-6xl text-indigo-600 mb-1" />
+              <span className="text-gray-800 text-xl font-bold">-5/+4</span>
+              <span className="text-gray-600 font-semibold">11 Mon</span>
+            </div>
+            <div className="flex flex-col items-center bg-gray-200 rounded-lg py-8 px-10 mr-12">
+              <WiSnow className="text-6xl text-indigo-600 mb-1" />
+              <span className="text-gray-800 text-xl font-bold">-5/+4</span>
+              <span className="text-gray-600 font-semibold">11 Mon</span>
+            </div>
+            <div className="flex flex-col items-center bg-gray-200 rounded-lg py-8 px-10 mr-12">
+              <WiDaySunnyOvercast className="text-6xl text-orange-400 mb-1" />
+              <span className="text-gray-800 text-xl font-bold">-5/+4</span>
+              <span className="text-gray-600 font-semibold">11 Mon</span>
+            </div>
+            <div className="flex flex-col items-center bg-gray-200 rounded-lg py-8 px-10 mr-12">
+              <WiCloudy className="text-6xl text-indigo-600 mb-1" />
+              <span className="text-gray-800 text-xl font-bold">-5/+4</span>
+              <span className="text-gray-600 font-semibold">11 Mon</span>
+            </div>
+            <div className="flex flex-col items-center bg-gray-200 rounded-lg py-8 px-10 mr-12">
+              <WiDaySunnyOvercast className="text-6xl text-orange-400 mb-1" />
+              <span className="text-gray-800 text-xl font-bold">-5/+4</span>
+              <span className="text-gray-600 font-semibold">11 Mon</span>
+            </div>
+            <div className="flex flex-col items-center bg-gray-200 rounded-lg py-8 px-10 mr-12">
+              <WiSnow className="text-6xl text-indigo-600 mb-1" />
+              <span className="text-gray-800 text-xl font-bold">-5/+4</span>
+              <span className="text-gray-600 font-semibold">11 Mon</span>
+            </div>
+            <div className="flex flex-col items-center bg-gray-200 rounded-lg py-8 px-10 mr-12">
+              <WiSnow className="text-6xl text-indigo-600 mb-1" />
+              <span className="text-gray-800 text-xl font-bold">-5/+4</span>
+              <span className="text-gray-600 font-semibold">11 Mon</span>
+            </div>
+            <div className="flex flex-col items-center bg-gray-200 rounded-lg py-8 px-10 mr-12">
+              <WiDaySunnyOvercast className="text-6xl text-orange-400 mb-1" />
+              <span className="text-gray-800 text-xl font-bold">-5/+4</span>
+              <span className="text-gray-600 font-semibold">11 Mon</span>
+            </div>
+          </div>
+        </section>
+        <section className="mb-8">
+          <h1 className="text-3xl font-bold text-gray-800 mb-4">
+            Weather graph
+          </h1>
+          <div className="bg-gray-200 rounded-lg p-6">
+            <Chart />
+          </div>
+        </section>
+      </main>
     </div>
   );
 }
---
+++ src/Chart.tsx
@@ -0,0 +1,121 @@
+import React from 'react';
+
+import {
+  LineChart,
+  Line,
+  XAxis,
+  YAxis,
+  CartesianGrid,
+  Tooltip,
+  ResponsiveContainer,
+} from 'recharts';
+
+const data = [
+  {
+    day: '11 Mon',
+    temp: -6,
+  },
+  {
+    day: '12 Tue',
+    temp: -10,
+  },
+  {
+    day: '13 Wed',
+    temp: -8,
+  },
+  {
+    day: '14 Thu',
+    temp: -5,
+  },
+  {
+    day: '15 Fri',
+    temp: 3,
+  },
+  {
+    day: '16 Sat',
+    temp: 6,
+  },
+  {
+    day: '17 Sun',
+    temp: 2,
+  },
+];
+
+const Chart = () => {
+  const { min, max } = data.reduce(
+    (result, dataPoint) => ({
+      min:
+        dataPoint.temp < result.min || result.min === 0
+          ? dataPoint.temp
+          : result.min,
+      max:
+        dataPoint.temp > result.max || result.max === 0
+          ? dataPoint.temp
+          : result.max,
+    }),
+    { min: 0, max: 0 },
+  );
+  const colorBreakPointPercentage = `${(1 - (0 - min) / (max - min)) * 100}%`;
+
+  const CustomLabel = (props: any) => {
+    const { x, y, stroke, value } = props;
+
+    return (
+      <text
+        x={x}
+        y={y - 20}
+        dy={-4}
+        fill={stroke}
+        fontSize={18}
+        textAnchor="middle"
+        fontFamily="system-ui"
+      >
+        {value}
+        &deg;C
+      </text>
+    );
+  };
+
+  return (
+    <ResponsiveContainer width="100%" height={380}>
+      <LineChart data={data}>
+        <defs>
+          <linearGradient id="colorUv" x1="0%" y1="0%" x2="0%" y2="100%">
+            <stop offset="0%" stopColor="#667EEA" />
+            <stop offset={colorBreakPointPercentage} stopColor="#667EEA" />
+            <stop offset={colorBreakPointPercentage} stopColor="#F56565" />
+            <stop offset="100%" stopColor="#F56565" />
+          </linearGradient>
+        </defs>
+        <XAxis
+          dataKey="day"
+          type="category"
+          axisLine={false}
+          tickLine={false}
+          tick={{
+            strokeWidth: 2,
+          }}
+          padding={{ left: 60, right: 60 }}
+        />
+        <YAxis
+          axisLine={false}
+          tickLine={false}
+          padding={{ top: 30, bottom: 30 }}
+        />
+        <CartesianGrid strokeDasharray="3 3" fill="#ffffff" strokeWidth={1} />
+        <Tooltip />
+        <Line
+          label={<CustomLabel />}
+          type="monotone"
+          dataKey="temp"
+          stroke="url(#colorUv)"
+          strokeWidth={4}
+          dot={false}
+          activeDot={false}
+        />
+      </LineChart>
+    </ResponsiveContainer>
+  );
+};
+
+export default Chart;
