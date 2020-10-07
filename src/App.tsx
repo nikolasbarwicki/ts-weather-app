26197a55cae2cf559402b6a11ce65b52e023ebb9
Nikolas Barwicki <nikolas.barwicki@gmail.com>
2020-10-07 07:56:27 +0200

    Fetch data for specific city

--- src/App.tsx
+++ src/App.tsx
@@ -7,7 +7,7 @@ import Sidebar from './components/Sidebar';
 import CitiesList from './components/CitiesList';
 import WeatherForecast from './components/WeatherForecast';
 import WeatherGraph from './components/WeatherGraph';
-import { WeatherData } from './types';
+import { WeatherData, CityList } from './types';
 
 function App() {
   const [weatherData, setWeatherData] = useState<WeatherData>({
@@ -18,6 +18,7 @@ function App() {
       windSpeed: 0,
       precipitation: 0,
       weather: 'Clear',
+      city: 'London',
     },
     daily: [
       {
@@ -29,44 +30,45 @@ function App() {
     ],
   });
 
+  const getWeather = async (lat: number, lon: number, activeCity: CityList) => {
+    const res = await axios.get(
+      `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&exclude=minutely,hourly,alerts&appid=9b50d0883faabc0bbbbd08bdaf9c1f57&units=metric`,
+    );
+    const { data } = res;
+    const { current, daily } = data;
+
+    const forecast = daily.map((day: any) => ({
+      dt: day.dt,
+      tempMax: day.temp.max,
+      tempMin: day.temp.min,
+      weather: day.weather[0].main,
+    }));
+
+    forecast.shift();
+
+    setWeatherData({
+      current: {
+        dt: current.dt,
+        humidity: current.humidity,
+        temp: current.temp,
+        windSpeed: current.wind_speed,
+        precipitation: current.rain || 0,
+        weather: current.weather[0].main,
+        city: activeCity,
+      },
+      daily: forecast,
+    });
+  };
+
   useEffect(() => {
-    const getWeather = async () => {
-      const res = await axios.get(
-        `https://api.openweathermap.org/data/2.5/onecall?lat=51.509865&lon=-0.118092&exclude=minutely,hourly,alerts&appid=9b50d0883faabc0bbbbd08bdaf9c1f57&units=metric`,
-      );
-      const { data } = res;
-      const { current, daily } = data;
-
-      const forecast = daily.map((day: any) => ({
-        dt: day.dt,
-        tempMax: day.temp.max,
-        tempMin: day.temp.min,
-        weather: day.weather[0].main,
-      }));
-
-      forecast.shift();
-
-      setWeatherData({
-        current: {
-          dt: current.dt,
-          humidity: current.humidity,
-          temp: current.temp,
-          windSpeed: current.wind_speed,
-          precipitation: current.rain || 0,
-          weather: current.weather[0].main,
-        },
-        daily: forecast,
-      });
-    };
-
-    getWeather();
+    getWeather(51.5, -0.12, 'London');
   }, []);
 
   return (
     <div className="flex">
       <Sidebar current={weatherData.current} />
       <main className=" container mx-auto flex flex-col mt-8">
-        <CitiesList />
+        <CitiesList getWeather={getWeather} />
         <WeatherForecast daily={weatherData.daily} />
         <WeatherGraph daily={weatherData.daily} />
       </main>
--- src/components/CitiesList.tsx
+++ src/components/CitiesList.tsx
@@ -1,67 +1,140 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import beijing from '../assets/images/beijing.jpg';
 import berlin from '../assets/images/berlin.jpg';
 import london from '../assets/images/london.jpg';
 import newyork from '../assets/images/newyork.jpg';
 import tokyo from '../assets/images/tokyo.jpg';
+import { CityList } from '../types';
 
-const CitiesList = () => {
+interface Props {
+  // eslint-disable-next-line no-unused-vars
+  getWeather: (lat: number, lon: number, activeCity: CityList) => Promise<void>;
+}
+
+const CitiesList: React.FC<Props> = ({ getWeather }) => {
+  const [activeCity, setActiveCity] = useState<CityList>('London');
+
   return (
     <section className="mb-8">
       <h1 className="text-3xl font-bold text-gray-800 mb-4">
         Weather Forecast
       </h1>
       <div className="flex justify-between">
-        <div className="flex flex-col items-center">
+        <button
+          className="flex flex-col items-center focus:outline-none"
+          onClick={() => {
+            getWeather(51.5, -0.12, 'London');
+            setActiveCity('London');
+          }}
+          type="button"
+        >
           <div className="h-40 w-56 rounded-lg relative overflow-auto">
             <img src={london} alt="" className="h-full " />
             <div className="bg-white text-gray-800 font-bold py-2 px-4 right-0 bottom-0 absolute rounded-tl-lg">
               12:31
             </div>
           </div>
-          <span className="text-indigo-600 text-xl font-bold mt-4">London</span>
-        </div>
+          <span
+            className={`${
+              activeCity === 'London' ? 'text-indigo-600' : 'text-gray-800'
+            } text-xl font-bold mt-4`}
+          >
+            London
+          </span>
+        </button>
 
-        <div className="flex flex-col items-center">
+        <button
+          className="flex flex-col items-center focus:outline-none"
+          onClick={() => {
+            getWeather(52.5, 13.4, 'Berlin');
+            setActiveCity('Berlin');
+          }}
+          type="button"
+        >
           <div className="h-40 w-56 rounded-lg relative overflow-auto">
             <img src={berlin} alt="" className="h-full " />
             <div className="bg-white text-gray-800 font-bold py-2 px-4 right-0 bottom-0 absolute rounded-tl-lg">
               13:31
             </div>
           </div>
-          <span className="text-gray-800 text-xl font-bold mt-4">Berlin</span>
-        </div>
+          <span
+            className={`${
+              activeCity === 'Berlin' ? 'text-indigo-600' : 'text-gray-800'
+            } text-xl font-bold mt-4`}
+          >
+            Berlin
+          </span>
+        </button>
 
-        <div className="flex flex-col items-center">
+        <button
+          className="flex flex-col items-center focus:outline-none"
+          onClick={() => {
+            getWeather(40.7, -73.9, 'New York');
+            setActiveCity('New York');
+          }}
+          type="button"
+        >
           <div className="h-40 w-56 rounded-lg relative overflow-auto">
             <img src={newyork} alt="" className="h-full " />
             <div className="bg-white text-gray-800 font-bold py-2 px-4 right-0 bottom-0 absolute rounded-tl-lg">
               13:31
             </div>
           </div>
-          <span className="text-gray-800 text-xl font-bold mt-4">New York</span>
-        </div>
+          <span
+            className={`${
+              activeCity === 'New York' ? 'text-indigo-600' : 'text-gray-800'
+            } text-xl font-bold mt-4`}
+          >
+            New York
+          </span>
+        </button>
 
-        <div className="flex flex-col items-center">
+        <button
+          className="flex flex-col items-center focus:outline-none"
+          onClick={() => {
+            getWeather(35.6, 139.8, 'Tokyo');
+            setActiveCity('Tokyo');
+          }}
+          type="button"
+        >
           <div className="h-40 w-56 rounded-lg relative overflow-auto">
             <img src={tokyo} alt="" className="h-full " />
             <div className="bg-white text-gray-800 font-bold py-2 px-4 right-0 bottom-0 absolute rounded-tl-lg">
               13:31
             </div>
           </div>
-          <span className="text-gray-800 text-xl font-bold mt-4">Tokyo</span>
-        </div>
+          <span
+            className={`${
+              activeCity === 'Tokyo' ? 'text-indigo-600' : 'text-gray-800'
+            } text-xl font-bold mt-4`}
+          >
+            Tokyo
+          </span>
+        </button>
 
-        <div className="flex flex-col items-center">
+        <button
+          className="flex flex-col items-center focus:outline-none"
+          onClick={() => {
+            getWeather(39.9, 116.3, 'Beijing');
+            setActiveCity('Beijing');
+          }}
+          type="button"
+        >
           <div className="h-40 w-56 rounded-lg relative overflow-auto">
             <img src={beijing} alt="" className="h-full " />
             <div className="bg-white text-gray-800 font-bold py-2 px-4 right-0 bottom-0 absolute rounded-tl-lg">
               13:31
             </div>
           </div>
-          <span className="text-gray-800 text-xl font-bold mt-4">Beijing</span>
-        </div>
+          <span
+            className={`${
+              activeCity === 'Beijing' ? 'text-indigo-600' : 'text-gray-800'
+            } text-xl font-bold mt-4`}
+          >
+            Beijing
+          </span>
+        </button>
       </div>
     </section>
   );
